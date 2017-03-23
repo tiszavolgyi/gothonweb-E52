540e71c0916f92d632a5b422255f469cffced1ab
Peter Tiszavolgyi <tiszavolgyi@gmail.com>
2017-03-23 08:16:33 -0700

    Game engine and basic web templates

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -5,17 +5,17 @@ from tests.tools import assert_response
 def test_index():
     #check that we get a 404 on the / urls
     resp = app.request("/")
-    assert_response(resp, status="404")
+    assert_response(resp, status="303")
 
     #test our first GET request to /hello
-    resp = app.request("/hello")
-    assert_response(resp)
+    #resp = app.request("/hello")
+    #assert_response(resp)
 
     # make sure default values work for the web form
-    resp = app.request("/hello", method="POST")
-    assert_response(resp, contains="Nobody")
+    #resp = app.request("/hello", method="POST")
+    #assert_response(resp, contains="Nobody")
 
     # test that we got the expected values
-    data = {'name': 'Peter', 'greet': 'Hello'}
-    resp = app.request("/hello", method="POST", data=data)
-    assert_response(resp, contains="Peter")
+    #data = {'name': 'Peter', 'greet': 'Hello'}
+    #resp = app.request("/hello", method="POST", data=data)
+    #assert_response(resp, contains="Peter")
